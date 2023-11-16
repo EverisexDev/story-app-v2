33eecc1c56929b34a67a747164f3c5fd3d53cffd
yenming <yenming0212@gmail.com>
2023-11-16 19:18:50 +0800

    renew

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./app/navigations/AppNavigator";
 
-
 export default function App() {
   return (
-
     <NavigationContainer>
-
-    <AppNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
-
-
-
-
